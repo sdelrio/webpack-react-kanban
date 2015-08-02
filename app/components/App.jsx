3b490b45d9bb6d7a110dc616b5f2f8c89220755d
Sergio del Rio <oftheriver@gmail.com>
2015-08-02 10:55:22 +0200

    Edit and Delete notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import uuid from 'node-uuid';
 import Notes from './Notes';
+import findIndex from '../libs/find_index';
 
 export default class App extends React.Component {
 
@@ -25,6 +26,7 @@ export default class App extends React.Component {
         };
 
         this.addItem = this.addItem.bind(this);
+        this.itemEdited = this.itemEdited.bind(this);
     }
 
     render() {
@@ -39,7 +41,7 @@ export default class App extends React.Component {
 
               <div>
                 <button onClick={this.addItem}>+</button>
-                <Notes items={notes} />
+                <Notes items={notes} onEdit={this.itemEdited} />
               </div>
 
                );
@@ -54,5 +56,24 @@ export default class App extends React.Component {
                 task: 'New task'
             }])
         });
+    }
+
+    itemEdited(noteId, task) {
+
+        let notes = this.state.notes;
+
+        const noteIndex = findIndex(notes, 'id', noteId);
+
+        if(noteIndex < 0) {
+            return console.warn('Failed to find note', notes, noteId);
+        }
+
+        if (task) {
+            notes[noteIndex].task = task;
+        } else {
+            notes = notes.slice(0, noteIndex).concat(notes.slice(noteIndex + 1));
+        }
+
+        this.setState({notes});
     }
 }
---
+++ app/libs/find_index.js
@@ -0,0 +1,7 @@
+export default function findIndex(arr, prop, value) {
+
+    const o = arr.filter(c => c[prop] === value)[0];
+    const ret = o && arr.indexOf(o);
+
+    return ret >= 0 ? ret : -1;
+}
