c45fbb8eb3302410b7ad7a2402783d953b5cb441
Sergio del Rio <oftheriver@gmail.com>
2015-08-02 10:15:57 +0200

    Add new task (no edit)

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,34 +3,56 @@ import uuid from 'node-uuid';
 import Notes from './Notes';
 
 export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            notes: [
+                   {
+                      id: uuid.v4(),
+                      task: 'Learn webpack'
+                   },
+                   {
+                      id: uuid.v4(),
+                      task: 'Learn React'
+                   },
+                   {
+                      id: uuid.v4(),
+                      task: 'Do laundry'
+                   }
+            ]
+        };
+
+        this.addItem = this.addItem.bind(this);
+    }
+
     render() {
 
-        /* Inmutable array of notes */
+        /* Nodes into state model */
 
-        const notes = [
-            {
-                id: uuid.v4(),
-                task: 'Learn webpack'
-            },
-            {
-                id: uuid.v4(),
-                task: 'Learn React'
-            },
-            {
-                id: uuid.v4(),
-                task: 'Do laundry'
-            }
-        ];
+        const notes = this.state.notes;
 
         /* Call componet Notes for render */
 
         return (
 
               <div>
+                <button onClick={this.addItem}>+</button>
                 <Notes items={notes} />
               </div>
 
                );
     }
 
+    addItem() {
+        console.log('add item');
+
+        this.setState({
+            notes: this.state.notes.concat([{
+                id: uuid.v4(),
+                task: 'New task'
+            }])
+        });
+    }
 }
